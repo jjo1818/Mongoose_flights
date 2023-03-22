ee2a71d5f8d9a94dfbca99b9f542a088129b8c81
jjo1818 <jenniferjoseph818@gmail.com>
2023-03-22 15:29:32 -0400

    second commit

---
+++ models/FlightModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+//Schema lets you create blueprints for the structure?
+const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema(
+  {
+    // expecting the "name" property to be a string. validation to make sure the data is legit
+    airport: { type: String, required: true, enum: ['AUS', 'DAL','LAX', 'SAN','SEA']}, 
+    arrival: { type: Date},
+  },
+);
+
+// Instantiate a new schema object for fruit and structour our data
+const flightSchema = new Schema(
+  {
+    // expecting the "name" property to be a string. validation to make sure the data is legit
+    airline: { type: String, required: true, enum: ['American', 'Southwest','United']}, 
+    flightNo: { type: Number, required: 10 <= 9999 },
+    departs: { type: Date, required: true },
+    airport: { type: String, required: true, enum: ['AUS', 'DAL','LAX', 'SAN','SEA','SAN']}, 
+    destinations: {type: [destinationSchema]},
+  },
+  { timestamps: true }
+  );
+
+
+// Creating our model using our fruitSchema and give our collection a name of "fruits"
+const Flight = mongoose.model('flight', flightSchema);
+
+const Destination = mongoose.model('destination', destinationSchema);
+
+// exporting the Flight model as a module
+module.exports = Flight
+
+// exporting the Destination model as a module
+module.exports = Destination;
---
+++ views/flights/New.jsx
@@ -0,0 +1,2 @@
+const now = newDate()
+    
